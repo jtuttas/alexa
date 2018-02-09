c778a58f245365e187bb69e4bc51ef3c0679ab85
jtuttas <jtuttas@gmx.net>
2018-02-09 14:51:41 +0100

    urldecode Kennwort und Benutzername

--- alexaauth.php
+++ alexaauth.php
@@ -2,18 +2,19 @@
 
 //echo "Username=".$_REQUEST["Username"];
 //echo "Password=".$_REQUEST["Password"];
-//error_log("Alexa state".$_REQUEST["state"]."\n", 3, "/var/tmp/errors.log");
-$url = $_REQUEST["Server"].$_REQUEST["Path"].'/login/token.php?username='.$_REQUEST["Username"].'&password='.$_REQUEST["Password"].'&service=moodle_mobile_app';
+error_log("Alexa state:".$_REQUEST["state"]."\n", 3, "/var/tmp/errors.log");
+error_log("Alexa redirect:".$_REQUEST["redirect_uri"]."\n", 3, "/var/tmp/errors.log");
+$url = $_REQUEST["Server"].$_REQUEST["Path"].'/login/token.php?username='.urldecode($_REQUEST["Username"]).'&password='.urldecode($_REQUEST["Password"]).'&service=moodle_mobile_app';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL,$url);
 $content = curl_exec($ch);
-
+echo $content;
 $array = json_decode($content, true); // decode json
 if ($array["token"]) {
 	$ch2 = curl_init();
 	$url = $_REQUEST["Server"].$_REQUEST["Path"].'/webservice/rest/server.php?wstoken='.$array["token"].'&wsfunction=core_webservice_get_site_info&moodlewsrestformat=json';
-	#echo "url=".$url;
+	//echo "url=".$url;
 	curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch2, CURLOPT_URL,$url);
 	$content2 = curl_exec($ch2);
